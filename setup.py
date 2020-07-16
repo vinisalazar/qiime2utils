60677a3b07cd9d97afdd71d02ad139915b211bd9
Vini Salazar <viniws@gmail.com>
2020-07-16 17:13:22 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="qiime2utils",
-    version="v0.0.8",
+    version="v0.0.9",
     author="Vini Salazar",
     author_email="viniws@gmail.com",
     description="qiime2utils - Utility scripts for Qiime 2",
